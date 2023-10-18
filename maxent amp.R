ba9c5c495e313c2ddb46b09997166b72e395fcb4
AljosaMilenkovic <96981129+AljosaMilenkovic@users.noreply.github.com>
2023-10-18 18:12:06 -0400

    Add files via upload

---
+++ maxent amp.R
@@ -0,0 +1,29 @@
+#MaxEnt analysis of the stress pattern of Gallipoli Serbian using the maxent.ot package (Mayer, Tan & Zuraw 2022)
+#Gallipoli Serbian is a now extinct dialect of BCMS. The analysis is based on the fieldwork data by Pavle Ivic (Ivic 1957/1994). A generative account of the dialect's stress pattern is provided in Milenkovic (2022)
+#Gallipoli Serbian consistently employs vowel lengthening to eliminate stressed toneless lights which form a monomoraic foot, but never lengthens High-toned lights that form a monomoraic foot.
+#Two models were compared: (1) the baseline model, and (2) the baseline model enriched with Head-H&FtBin (the LCC proposed in the present survey)
+
+library(maxent.ot)
+
+#input files
+baseline_url <- "https://raw.githubusercontent.com/AljosaMilenkovic/amp-2023-supplementary-material/main/baseline_maxent.txt"
+lcc_url <- "https://raw.githubusercontent.com/AljosaMilenkovic/amp-2023-supplementary-material/main/lcc_maxent.txt"
+
+baseline <- read.csv(baseline_url, header = 1, sep = "\t", encoding = "UTF-8")
+lcc <- read.csv(lcc_url, header = 1, sep = "\t", encoding = "UTF-8")
+
+###########################
+#Models
+baseline_model <- optimize_weights(baseline)
+lcc_model <- optimize_weights(lcc)
+
+#Constraint weights
+baseline_model$weights
+lcc_model$weights
+
+#log likelihood
+baseline_model$loglik
+lcc_model$loglik
+
+#likelihood ratio test
+compare_models(baseline_model, lcc_model, method = 'lrt')
